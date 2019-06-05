b9759330b9f8cade3a09c0446d7c44f51add2341
Ed Allen <45358248+NeutronCat@users.noreply.github.com>
2019-06-05 23:23:46 +0100

    Update Loops_Basic.py

---
+++ Loops_Basic.py
@@ -0,0 +1,11 @@
+#simple
+dog_breeds = ['french_bulldog', 'dalmatian', 'shihtzu', 'poodle', 'collie']
+for breed in dog_breeds:
+    print(breed)
+
+#simple range
+promise = "I will not chew gum in class"
+for n in range(5):
+  print(promise)
+  
+
