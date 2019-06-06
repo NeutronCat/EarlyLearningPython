aca96a3e9f32f5ca9605fdcc63bd9e792e8a0810
Ed Allen <45358248+NeutronCat@users.noreply.github.com>
2019-06-06 18:37:48 +0100

    Update Loops_Challenge.py

--- Loops_Challenge.py
+++ Loops_Challenge.py
@@ -9,7 +9,21 @@ def divisible_by_ten(nums):
   return count
 print(divisible_by_ten([20, 25, 30, 35, 40]))
 
+#Geetings
+def add_greetings(names):
+  greeting = []
+  for name in names:
+    greeting.append("Hello, "+name)
+  return greeting
+print(add_greetings(["Owen", "Max", "Sophie"]))
 
+#delete starting even 
+def delete_starting_evens(lst):
+  while (len(lst) > 0 and lst[0] % 2 == 0):
+    lst = lst[1:]
+  return lst
+print(delete_starting_evens([4, 8, 10, 11, 12, 15]))
+print(delete_starting_evens([4, 8, 10]))
 
 
 
