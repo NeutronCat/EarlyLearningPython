b5af4dc502fa315719e8f6011248a2166ab0ee22
Ed Allen <45358248+NeutronCat@users.noreply.github.com>
2019-06-06 13:54:50 +0100

    Create Loops_Challenge.py

---
+++ Loops_Challenge.py
@@ -0,0 +1,16 @@
+# loops challenge!
+
+#count of nums divisible by ten
+def divisible_by_ten(nums):
+  count = 0
+  for number in nums:
+    if (number % 10 == 0):
+      count += 1
+  return count
+print(divisible_by_ten([20, 25, 30, 35, 40]))
+
+
+
+
+
+
