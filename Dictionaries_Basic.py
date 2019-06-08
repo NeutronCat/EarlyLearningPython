fc4022d5773b17b8eb3aa2e6c2897d5dc1c3a028
Ed Allen <45358248+NeutronCat@users.noreply.github.com>
2019-06-08 20:42:08 +0100

    Update Dictionaries_Basic.py

--- Dictionaries_Basic.py
+++ Dictionaries_Basic.py
@@ -51,8 +51,18 @@ print(oscar_winners)
 
 #List Comprehensions to Dictionaries
 #Python allows you to create a dictionary using a list comprehension
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+zipped_drinks = zip(drinks,caffeine)
+drinks_to_caffeine = {drinks:caffeine for drinks,caffeine in zip(drinks,caffeine)}
+print(drinks_to_caffeine)
 
-
-
-
-
+#review
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+plays = {song:playcount for song, playcount in zip(songs,playcounts)}
+print(plays)
+plays.update({"Purple Haze": 1})
+plays["Respect"] = 94
+library = {"The Best Songs": plays, "Sunday Feelings": {}}
+print(library)
