5031d1d74f2efeeb9b35e10d0e48fc0427f4d0d6
Ed Allen <45358248+NeutronCat@users.noreply.github.com>
2019-06-08 19:26:37 +0100

    Update Modules_BasicUse.py

--- Modules_BasicUse.py
+++ Modules_BasicUse.py
@@ -26,5 +26,21 @@ numbers_b = random.sample(range(1000), 12)
 plt.plot(numbers_a,numbers_b)
 plt.show()
 
+#Decimal from decimal module
+from decimal import Decimal
+two_decimal_points = Decimal('0.2') + Decimal('0.69')
+print(two_decimal_points)
+four_decimal_points = Decimal('0.53') * Decimal('0.65')
+print(four_decimal_points)
+
+#files are modules, modules are files
+# in library.py
+def always_three():
+  return 3
+# in script.py
+from library import always_three
+print(always_three())
+
+
 
 
