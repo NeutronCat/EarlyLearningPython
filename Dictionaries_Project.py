ab85a4715c50244caed21972d3362c2c09bb17a0
Ed Allen <45358248+NeutronCat@users.noreply.github.com>
2019-06-08 22:30:13 +0100

    Update Dictionaries_Project.py

--- Dictionaries_Project.py
+++ Dictionaries_Project.py
@@ -19,11 +19,36 @@ for player, words in player_to_words.items():
     player_points += score_word(word)
   player_to_points[player] = player_points
 
-def play_word(player,word):
-  
+def play_word(player,word): #adds word to player
+  player_to_words[player].append(word)
 
 print(letter_to_points)
 print(brownie_points)
 print(player_to_points)
+
+# Write a function named sum_values that takes a dictionary named my_dictionary as a parameter. The function should return the sum of the values of the dictionary
+def sum_values(my_dictionary):
+  total = 0
+  for value in my_dictionary.values():
+    total += value
+  return total
+print(sum_values({"milk":5, "eggs":2, "flour": 3}))
+# should print 10
+print(sum_values({10:1, 100:2, 1000:3}))
+# should print 6
+
+# Create a function called sum_even_keys that takes a dictionary named my_dictionary, with all integer keys and values, as a parameter. This function should return the sum of the values of all even keys.
+
+def sum_even_keys(my_dictionary):
+  total = 0
+  for key in my_dictionary.keys():
+    if key%2 == 0:
+      total += my_dictionary[key]
+  return total
+print(sum_even_keys({1:5, 2:2, 3:3}))
+# should print 2
+print(sum_even_keys({10:1, 100:2, 1000:3}))
+# should print 6
+
 
 
