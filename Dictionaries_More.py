a1bf498ffa7b517e734a738ac240ed7fe026ee86
Ed Allen <45358248+NeutronCat@users.noreply.github.com>
2019-06-08 20:49:19 +0100

    Create Dictionaries_More.py

---
+++ Dictionaries_More.py
@@ -0,0 +1,9 @@
+#Using Dictionaries
+# Use a key to get a value from a dictionary
+# Check for existence of keys
+# Find the length of a dictionary
+# Iterate through keys and values in dictionaries
+
+
+
+
